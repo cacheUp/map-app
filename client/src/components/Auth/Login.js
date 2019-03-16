90d7fe538c43cc39c0ccd3a414715bccf49145a9
cacheUp <bradb26@gmail.com>
2019-03-16 22:34:09 +0300

    added getAuthResponse to ged id token

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -5,13 +5,15 @@ import { withStyles } from "@material-ui/core/styles";
 
 const Login = ({ classes }) => {
   const onSuccess = googleUser => {
-    console.log({ googleUser });
+    const idToken = googleUser.getAuthResponse().id_token;
+    console.log({ idToken });
   };
 
   return (
     <GoogleLogin
       clientId="895850204586-sju6t2c25jpm90ksv8u3tv83bvocead2.apps.googleusercontent.com"
       onSuccess={onSuccess}
+      isSignedIn={true}
     />
   );
 };
