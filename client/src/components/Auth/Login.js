7365c79b93020c4096e712411a645037e180519e
cacheUp <bradb26@gmail.com>
2019-03-16 22:14:18 +0300

    added google client login

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
 
 const Login = ({ classes }) => {
-  return <div>Login</div>;
+  const onSuccess = googleUser => {
+    console.log({ googleUser });
+  };
+
+  return (
+    <GoogleLogin
+      clientId="895850204586-sju6t2c25jpm90ksv8u3tv83bvocead2.apps.googleusercontent.com"
+      onSuccess={onSuccess}
+    />
+  );
 };
 
 const styles = {
