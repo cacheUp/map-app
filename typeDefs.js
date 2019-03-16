c865b49d54189be867cdbb002a607ee04347188c
cacheUp <bradb26@gmail.com>
2019-03-16 21:01:07 +0300

    added Pin model and typeDef

--- typeDefs.js
+++ typeDefs.js
@@ -7,6 +7,23 @@ module.exports = gql`
     email: String
     picture: String
   }
+  type Pin {
+    _id: ID
+    createdAt: String
+    title: String
+    content: String
+    image: String
+    latitude: Float
+    longitude: Float
+    author: Float
+    comments: [Comment]
+  }
+
+  type Comment {
+    text: String
+    createdAt: String
+    author: User
+  }
 
   type Query {
     me: User
